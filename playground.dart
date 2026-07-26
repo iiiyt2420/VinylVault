991f90709fc1e1bd07de6560fbcfd697284a2728
Oleksandr <iiiyt2420@gmail.com>
2026-07-26 21:43:30 +0300

    changed types to final

--- playground.dart
+++ playground.dart
@@ -2,9 +2,9 @@ void main() {
   final albumTitle = 'Number Ones [2LP]';
   final artistName = 'Michael Jackson';
   final yearReleased = 2003;
-  var price = 3240.0;
-  var isAvailable = true;
-  var quantityInStock = 5;
+  final price = 3240.0;
+  final isAvailable = true;
+  final quantityInStock = 5;
   print('Альбом: $albumTitle, Виконавець: $artistName, Рік випуску: $yearReleased, Ціна: $price, Наявність: $isAvailable, Кількість на складі: $quantityInStock');
   print('ціна зі знижкою: ${price * 0.8}');
 }
