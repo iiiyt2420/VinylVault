72366b7bc6930fd22f893caf28f082694df5a0b6
Oleksandr <iiiyt2420@gmail.com>
2026-07-30 20:34:49 +0300

    Add operators practice with duration calculations

--- operators.dart
+++ operators.dart
@@ -1,25 +1,22 @@
 void main() {
-  final albumTitle = 'Number Ones [2LP]';
-  final artistName = 'Michael Jackson';
-  final yearReleased = 2003;
+  const moreThan = 45;
   final price = 3240.0;
   final isAvailable = true;
-  final quantityInStock = 5;
   final timeSeconds = 4732;
   final trackCount = 18;
   final timeMinutes = timeSeconds ~/ 60;
-  final timeRemaining = timeSeconds % 60;
+  final remainingSeconds = timeSeconds % 60;
   final timePerTrack = timeSeconds ~/ trackCount;
   final costPerTrack = price / trackCount;
-  final isLongPlay = timeSeconds > 2700;
-  final payNow = isAvailable && price < 4000.0;
-  final discounted = isLongPlay || trackCount > 15;
-  print('$timeMinutes хвилин $timeRemaining секунди');
+  final isLongPlay = timeSeconds > moreThan * 60;
+  final canBuyNow = isAvailable && price < 4000.0;
+  final hasDiscount = isLongPlay || trackCount > 15;
+  print('$timeMinutes хвилин $remainingSeconds секунди');
   print('Середня тривалість треку: $timePerTrack секунди');
-  print('Вартість кожного треку: $costPerTrack');
+  print('Вартість кожного треку: ${costPerTrack.toStringAsFixed(2)}');
   print('Чи є це довгим плейтом? $isLongPlay');
-  print('Чи можна купити зараз? $payNow');
-  print('Чи діє акція? $discounted');
+  print('Чи можна купити зараз? $canBuyNow');
+  print('Чи діє акція? $hasDiscount');
 
   print(40 / 8);
   print(40 ~/ 8);
