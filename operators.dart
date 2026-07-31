b404cfa944c68b2a0f797299a51a467ec65a9aaa
Oleksandr <iiiyt2420@gmail.com>
2026-07-31 18:51:30 +0300

    Refactor operators naming and remove unused variables

--- operators.dart
+++ operators.dart
@@ -1,5 +1,7 @@
 void main() {
-  const moreThan = 45;
+  const longPlayMinutes = 45;
+  const moreTracksCount = 15;
+  const lowerPrice = 4000.0;
   final price = 3240.0;
   final isAvailable = true;
   final timeSeconds = 4732;
@@ -8,9 +10,9 @@ void main() {
   final remainingSeconds = timeSeconds % 60;
   final timePerTrack = timeSeconds ~/ trackCount;
   final costPerTrack = price / trackCount;
-  final isLongPlay = timeSeconds > moreThan * 60;
-  final canBuyNow = isAvailable && price < 4000.0;
-  final hasDiscount = isLongPlay || trackCount > 15;
+  final isLongPlay = timeSeconds > longPlayMinutes * 60;
+  final canBuyNow = isAvailable && price < lowerPrice;
+  final hasDiscount = isLongPlay || trackCount > moreTracksCount;
   print('$timeMinutes хвилин $remainingSeconds секунди');
   print('Середня тривалість треку: $timePerTrack секунди');
   print('Вартість кожного треку: ${costPerTrack.toStringAsFixed(2)}');
